4dd382b702cb7a1d242c5c1bd6a6e6f48076631b
TheoJoha <96450966+TheoJoha@users.noreply.github.com>
2023-02-16 11:05:27 +0100

    Add files via upload

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -13,7 +13,23 @@ const totalLikes = (blogs) => {
   return likes
 }
 
+const blogWithMostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  let mostLikes = 0
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes >= mostLikes) {
+      mostLikes = i
+    }
+  }
+  return blogs[mostLikes]
+
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  blogWithMostLikes
 }
