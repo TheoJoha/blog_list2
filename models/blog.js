29425967964543fdf169a7e6ab5e937093d83799
TheoJoha <96450966+TheoJoha@users.noreply.github.com>
2023-02-28 00:06:48 +0100

    Add files via upload

--- models/blog.js
+++ models/blog.js
@@ -25,9 +25,13 @@ mongoose.connect(url)
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  url: String,
-  likes: { type: Number, default: 0 }
+  author: String,
+  url: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 /* noteSchema.set('toJSON', {
